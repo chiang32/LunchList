33456a814c059d1e51aa32d74a7bbf5f88187ca8
Jarod Chiang <jarod.chiang@twinova.com>
2012-03-14 17:23:14 +0800

    w

--- gen/apt/tutorial/R.java
+++ gen/apt/tutorial/R.java
@@ -15,6 +15,8 @@ public final class R {
         public static final int ball_red=0x7f020001;
         public static final int ball_yellow=0x7f020002;
         public static final int ic_launcher=0x7f020003;
+        public static final int list=0x7f020004;
+        public static final int restaurant=0x7f020005;
     }
     public static final class id {
         public static final int addr=0x7f050002;
