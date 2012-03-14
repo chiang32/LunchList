4dba9aa4723eccf15f2fe6bb1a020222e0602574
Jarod Chiang <jarod.chiang@twinova.com>
2012-03-14 14:07:52 +0800

    ch4 adding list.

--- gen/apt/tutorial/R.java
+++ gen/apt/tutorial/R.java
@@ -13,6 +13,17 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int addr=0x7f050002;
+        public static final int delivery=0x7f050006;
+        public static final int details=0x7f050000;
+        public static final int name=0x7f050001;
+        public static final int restaurants=0x7f050008;
+        public static final int save=0x7f050007;
+        public static final int sit_down=0x7f050005;
+        public static final int take_out=0x7f050004;
+        public static final int types=0x7f050003;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
--- src/apt/tutorial/LunchListActivity.java
+++ src/apt/tutorial/LunchListActivity.java
@@ -1,14 +1,129 @@
 package apt.tutorial;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioGroup;
 
 public class LunchListActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // yes it is.
-        setContentView(R.layout.main);
-    }
+	List<Restaurant> model=new ArrayList<Restaurant>();
+	ArrayAdapter<Restaurant> adapter=null;
+
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		Button save = (Button) findViewById(R.id.save);
+		save.setOnClickListener(onSave);
+		
+	  
+	    
+		ListView list = (ListView)findViewById(R.id.restaurants);
+		adapter = new ArrayAdapter<Restaurant>(this, android.R.layout.simple_list_item_1, model);
+		//adapter = new ArrayAdapter<Restaurant>(this, android.R.layout.restaurants, model);
+			
+	    list.setAdapter(adapter);
+
+	    Restaurant r3 = new Restaurant();
+	    r3.setAddress("aaaa3");
+	    r3.setName( "name3");
+	    r3.setType("sit_down");
+	    adapter.add( r3);
+//	    
+//	    Restaurant r1 = new Restaurant();
+//	    r1.setAddress("aaaa");
+//	    r1.setName( "name1");
+//	    r1.setType("sit_down");
+//	    adapter.add( r1);
+//
+//	    Restaurant r2 = new Restaurant();
+//	    r2.setAddress("aaaa2");
+//	    r2.setName( "name2");
+//	    r2.setType("sit_down");
+//	    adapter.add(r2);
+
+	    
+	  
+	   
+	}
+
+	private View.OnClickListener onSave = new View.OnClickListener() {
+		public void onClick(View v) {
+			Restaurant r = new Restaurant();
+			
+			EditText name = (EditText) findViewById(R.id.name);
+			EditText address = (EditText) findViewById(R.id.addr);
+
+			r.setName(name.getText().toString());
+			r.setAddress(address.getText().toString());
+			
+			RadioGroup types = (RadioGroup) findViewById(R.id.types);
+
+			switch (types.getCheckedRadioButtonId()) {
+			case R.id.sit_down:
+				r.setType("sit_down");
+				break;
+			case R.id.take_out:
+				r.setType("take_out");
+				break;
+			case R.id.delivery:
+				r.setType("delivery");
+				break;
+			}
+//			adapter.add(r);
+		adapter.add(r);
+			  appendLog( "counts are "+ adapter.getCount() );
+		}
+
+	};
+	
+	/**
+	 * @description write logs
+	 * @param text
+	 */
+	public void appendLog(String text)
+	{ 
+       String path = "/mnt/sdcard/jarod.log";
+	   File logFile = new File(path);
+	   if (!logFile.exists())
+	   {
+	      try
+	      {
+	         logFile.createNewFile();
+	      } 
+	      catch (IOException e)
+	      {
+	         // TODO Auto-generated catch block
+	         e.printStackTrace();
+	      }
+	   }
+	   try
+	   {
+	      //BufferedWriter for performance, true to set append to file flag
+	      BufferedWriter buf = new BufferedWriter(new FileWriter(logFile, true)); 
+	      buf.append(text);
+	      buf.newLine();
+	      buf.close();
+	   }
+	   catch (IOException e)
+	   {
+	      // TODO Auto-generated catch block
+	      e.printStackTrace();
+	   }
+	}
+
 }
