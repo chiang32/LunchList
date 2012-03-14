57e9fa0d4aad3ea8ad699ac0ed42c6c3fee34726
Jarod Chiang <jarod.chiang@twinova.com>
2012-03-14 15:45:55 +0800

    get ready for branch.

--- src/apt/tutorial/LunchListActivity.java
+++ src/apt/tutorial/LunchListActivity.java
@@ -117,40 +117,41 @@ public class LunchListActivity extends Activity {
 
 			if (row == null) {
 				LayoutInflater inflater = getLayoutInflater();
-				
-				row = inflater.inflate( R.layout.row, parent, false);
+
+				row = inflater.inflate(R.layout.row, parent, false);
 				holder = new RestaurantHolder(row);
 				row.setTag(holder);
 			} else {
-				holder = ( RestaurantHolder) row.getTag();
-				}
-				holder.populateFrom(model.get(position));
+				holder = (RestaurantHolder) row.getTag();
+			}
+			holder.populateFrom(model.get(position));
 			return (row);
 
 		}
 
 	}
+
 	static class RestaurantHolder {
-		private TextView name=null;
-		private TextView address=null;
-		private ImageView icon=null;
+		private TextView name = null;
+		private TextView address = null;
+		private ImageView icon = null;
+
 		RestaurantHolder(View row) {
-		name = (TextView)row.findViewById(R.id.title);
-		address = (TextView)row.findViewById(R.id.address);
-		icon = (ImageView)row.findViewById(R.id.icon);
+			name = (TextView) row.findViewById(R.id.title);
+			address = (TextView) row.findViewById(R.id.address);
+			icon = (ImageView) row.findViewById(R.id.icon);
 		}
+
 		void populateFrom(Restaurant r) {
-		name.setText(r.getName());
-		address.setText(r.getAddress());
-		if (r.getType().equals("sit_down")) {
-		icon.setImageResource(R.drawable.ball_red);
-		}
-		else if (r.getType().equals("take_out")) {
-		icon.setImageResource(R.drawable.ball_yellow);
-		}
-		else {
-		icon.setImageResource(R.drawable.ball_green);
-		}
+			name.setText(r.getName());
+			address.setText(r.getAddress());
+			if (r.getType().equals("sit_down")) {
+				icon.setImageResource(R.drawable.ball_red);
+			} else if (r.getType().equals("take_out")) {
+				icon.setImageResource(R.drawable.ball_yellow);
+			} else {
+				icon.setImageResource(R.drawable.ball_green);
+			}
 		}
 	}
 }
