677f8d2ce3988c5a9092ccecc7729df0fe43a242
Jarod Chiang <jarod.chiang@twinova.com>
2012-03-14 14:13:08 +0800

    model added.

---
+++ src/apt/tutorial/Restaurant.java
@@ -0,0 +1,46 @@
+package apt.tutorial;
+
+
+public class Restaurant {
+	private String name = "";
+	private String address = "";
+	private String type = "";
+	Restaurant(){
+		
+	}
+	Restaurant( String aName, String aAddress, String aType) {
+		name = aName;
+		address = aAddress;
+		type = aType;
+
+	}
+
+	public String getType() {
+		return (type);
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return (name);
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return (address);
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String toString() {
+		return (getName());
+	}
+
+}
