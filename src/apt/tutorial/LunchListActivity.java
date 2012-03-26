9ac48e289d20b930a4d1cdc32184eb64b8822956
Jarod Chiang <jarod.chiang@twinova.com>
2012-03-26 14:19:11 +0800

    y

--- src/apt/tutorial/LunchListActivity.java
+++ src/apt/tutorial/LunchListActivity.java
@@ -9,19 +9,13 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 import android.app.TabActivity;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -31,7 +25,6 @@ import android.widget.ListView;
 import android.widget.RadioGroup;
 import android.widget.TabHost;
 import android.widget.TextView;
-import android.widget.Toast;
 
 /**
  * @author Jarod Chiang
@@ -45,41 +38,14 @@ public class LunchListActivity extends TabActivity {
 	EditText notes = null;
 	RadioGroup types = null;
 	Restaurant current = null;
-	int progress = 0;
+    RestaurantHelper helper=null;
 	private static final String LOG_KEY = "TEST";
-	AtomicBoolean isActive = new AtomicBoolean(true);
-
-	private Runnable longTask = new Runnable() {
-
-		public void run() {
-			for (int i = progress; i < 10000 && isActive.get(); i += 200) {
-				doSomeLongWork(200);
-			}
-			if( isActive.get() ){
-				runOnUiThread(new Runnable() {
-					@Override
-					public void run() {
-						setProgressBarVisibility(false);
-						progress = 0;
-					}
-				});				
-			}
-
-		}
-	};
-
 	
-
-	// ArrayAdapter<Restaurant> adapter = null;
-
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_PROGRESS);
-
 		setContentView(R.layout.main);
-
 		name = (EditText) findViewById(R.id.name);
 		address = (EditText) findViewById(R.id.addr);
 		types = (RadioGroup) findViewById(R.id.types);
@@ -102,8 +68,7 @@ public class LunchListActivity extends TabActivity {
 
 		spec = getTabHost().newTabSpec("tag2");
 		spec.setContent(R.id.details);
-		spec.setIndicator("Details",
-				getResources().getDrawable(R.drawable.restaurant));
+		spec.setIndicator("Details", getResources().getDrawable(R.drawable.restaurant));
 
 		getTabHost().addTab(spec);
 		getTabHost().setCurrentTab(0);
@@ -114,73 +79,11 @@ public class LunchListActivity extends TabActivity {
 			public void onClick(View v) {
 				appendLog("OnClick");
 				Log.d(LOG_KEY, "OnClick");
-				// Toast.makeText(this, "message", Toast.LENGTH_LONG).show();
-
-				// if (getTabHost().getCurrentTabTag().equals( myTabTag )) {
-				// getTabHost().setCurrentTab(myTabIndex);
-				// }
 			}
 		});
 
 	}
-	@Override
-	public void onPause() {
-		super.onPause();
-		isActive.set(false);
-	}
 
-	@Override
-	public void onResume() {
-		super.onResume();
-		isActive.set(true);
-		if (progress > 0) {
-			startWork();
-		}
-	}
-
-	private void startWork() {
-		setProgressBarVisibility(true);
-		new Thread(longTask).start();
-	}
-	private void doSomeLongWork(final int incr) {
-		runOnUiThread(new Runnable() {
-			@Override
-			public void run() {
-				progress += incr;
-				setProgress(progress);
-			}
-		});
-		SystemClock.sleep(250); // should be something more useful!
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		new MenuInflater(this).inflate(R.menu.option, menu);
-		return (super.onCreateOptionsMenu(menu));
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-
-		if (item.getItemId() == R.id.toast) {
-			
-			String message = "No restaurant selected";
-			if (current != null) {
-				message = current.getNotes();
-			}
-			Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-			return (true);
-			
-		} else if (item.getItemId() == R.id.run) {
-			startWork();
-//			Toast.makeText(this, "completed...", Toast.LENGTH_LONG).show();
-			return (true);
-		}
-
-		// Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-		return (super.onOptionsItemSelected(item));
-
-	}
 
 	private View.OnClickListener onSave = new View.OnClickListener() {
 		public void onClick(View v) {
