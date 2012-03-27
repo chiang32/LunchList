0e7971f2dcecad25ad434a5c9d6b7fcad2351dd8
Jarod Chiang <jarod.chiang@twinova.com>
2012-03-28 00:15:55 +0800

    syncTask.Intent.rss.

--- src/apt/tutorial/DetailForm.java
+++ src/apt/tutorial/DetailForm.java
@@ -1,21 +1,30 @@
 package apt.tutorial;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class DetailForm extends Activity {
 	EditText name = null;
 	EditText address = null;
 	EditText notes = null;
+	EditText feed = null;
 	RadioGroup types = null;
 
 	RestaurantHelper helper = null;
 	String restaurantId = null;
+	
 	private View.OnClickListener onSave = new View.OnClickListener() {
 		@Override
 		public void onClick(View v) {
@@ -36,27 +45,29 @@ public class DetailForm extends Activity {
 			}
 			if (restaurantId == null) {
 				helper.insert(name.getText().toString(), address.getText()
-						.toString(), type, notes.getText().toString());
+						.toString(), type, notes.getText().toString(), feed
+						.getText().toString());
 			} else {
-				helper.update(restaurantId, name.getText().toString(), address.getText()
-						.toString(), type, notes.getText().toString());
+				helper.update(restaurantId, name.getText().toString(), address
+						.getText().toString(), type,
+						notes.getText().toString(), feed.getText().toString());
 			}
 			finish();
 		}
 	};
 
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-setContentView(R.layout.detail_form);
+		setContentView(R.layout.detail_form);
 		helper = new RestaurantHelper(this);
 
 		name = (EditText) findViewById(R.id.name);
 		address = (EditText) findViewById(R.id.addr);
 		types = (RadioGroup) findViewById(R.id.types);
 		notes = (EditText) findViewById(R.id.notes);
+		feed = (EditText) findViewById(R.id.feed);
 
 		Button save = (Button) findViewById(R.id.save);
 
@@ -68,31 +79,70 @@ setContentView(R.layout.detail_form);
 		}
 
 	}
+
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
 		helper.close();
 	}
-	  @Override
-	  public void onSaveInstanceState(Bundle state) {
-	    super.onSaveInstanceState(state);
-	    
-	    state.putString("name", name.getText().toString());
-	    state.putString("address", address.getText().toString());
-	    state.putString("notes", notes.getText().toString());
-	    state.putInt("type", types.getCheckedRadioButtonId());
-	  }
 
-	  @Override
-	  public void onRestoreInstanceState(Bundle state) {
-	    super.onRestoreInstanceState(state);
-	    
-	    name.setText(state.getString("name"));
-	    address.setText(state.getString("address"));
-	    notes.setText(state.getString("notes"));
-	    types.check(state.getInt("type"));
-	  }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		new MenuInflater(this).inflate(R.menu.details_option, menu);
 
+		return (super.onCreateOptionsMenu(menu));
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.feed) {
+			if (isNetworkAvailable()) {
+				
+	
+						
+			Intent i = new Intent(this, FeedActivity.class);
+				i.putExtra(FeedActivity.FEED_URL, feed.getText().toString());
+		//		Toast.makeText(this, "Sorry, "+feed.getText().toString(), Toast.LENGTH_LONG).show();
+					startActivity(i);
+			} else {
+				Toast.makeText(this, "Sorry, the Internet is not available",
+						Toast.LENGTH_LONG).show();
+			}
+
+			return (true);
+		}
+
+		return (super.onOptionsItemSelected(item));
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle state) {
+		super.onSaveInstanceState(state);
+
+		state.putString("name", name.getText().toString());
+		state.putString("address", address.getText().toString());
+		state.putString("notes", notes.getText().toString());
+		state.putString("feed", feed.getText().toString());
+		state.putInt("type", types.getCheckedRadioButtonId());
+	}
+
+	@Override
+	public void onRestoreInstanceState(Bundle state) {
+		super.onRestoreInstanceState(state);
+
+		name.setText(state.getString("name"));
+		address.setText(state.getString("address"));
+		notes.setText(state.getString("notes"));
+		feed.setText(state.getString("feed"));
+		types.check(state.getInt("type"));
+	}
+
+	private boolean isNetworkAvailable() {
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+		NetworkInfo info = cm.getActiveNetworkInfo();
+
+		return (info != null);
+	}
 
 	private void load() {
 		Cursor c = helper.getById(restaurantId);
@@ -101,6 +151,7 @@ setContentView(R.layout.detail_form);
 		name.setText(helper.getName(c));
 		address.setText(helper.getAddress(c));
 		notes.setText(helper.getNotes(c));
+		feed.setText(helper.getFeed(c));
 
 		if (helper.getType(c).equals("sit_down")) {
 			types.check(R.id.sit_down);
--- src/apt/tutorial/RestaurantHelper.java
+++ src/apt/tutorial/RestaurantHelper.java
@@ -8,7 +8,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 class RestaurantHelper extends SQLiteOpenHelper {
 	private static final String DATABASE_NAME = "lunchlist.db";
-	private static final int SCHEMA_VERSION = 1;
+	private static final int SCHEMA_VERSION = 2;
 
 	public RestaurantHelper(Context context) {
 		super(context, DATABASE_NAME, null, SCHEMA_VERSION);
@@ -16,28 +16,32 @@ class RestaurantHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		db.execSQL("CREATE TABLE restaurants (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, type TEXT, notes TEXT);");
+		db.execSQL(
+				"CREATE TABLE restaurants ("
+		+"_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+		+"name TEXT, address TEXT, type TEXT, notes TEXT feed TEXT);");
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// no-op, since will not be called until 2nd schema
 		// version exists
-
+	    db.execSQL("ALTER TABLE restaurants ADD COLUMN feed TEXT");
 	}
 
-	public void insert(String name, String address, String type, String notes) {
+	public void insert(String name, String address, String type, String notes, String feed) {
 		ContentValues cv = new ContentValues();
 
 		cv.put("name", name);
 		cv.put("address", address);
 		cv.put("type", type);
 		cv.put("notes", notes);
+		cv.put("feed", feed);
 
 		getWritableDatabase().insert("restaurants", "name", cv);
 	}
 
-	public void update(String id, String name, String address, String type, String notes) {
+	public void update(String id, String name, String address, String type, String notes, String feed) {
 		ContentValues cv = new ContentValues();
 		String[] args = { id };
 
@@ -45,6 +49,8 @@ class RestaurantHelper extends SQLiteOpenHelper {
 		cv.put("address", address);
 		cv.put("type", type);
 		cv.put("notes", notes);
+		cv.put("feed", feed);
+		
 
 		getWritableDatabase().update("restaurants", cv, "_ID=?", args);
 	}
@@ -52,7 +58,7 @@ class RestaurantHelper extends SQLiteOpenHelper {
 	public Cursor getAll(String orderBy) {
 		return (getReadableDatabase()
 				.rawQuery(						
-						"SELECT _id, name, address, type, notes FROM restaurants ORDER BY "+orderBy,
+						"SELECT _id, name, address, type, notes, feed FROM restaurants ORDER BY "+orderBy,
 						null));
 	}
 
@@ -71,12 +77,14 @@ class RestaurantHelper extends SQLiteOpenHelper {
 	public String getNotes(Cursor c) {
 		return (c.getString(4));
 	}
-
+	public String getFeed(Cursor c) {
+		return (c.getString(5));
+	}
 	public Cursor getById(String id) {
 		String[] args = { id };
 		return (getReadableDatabase()
 				.rawQuery(
-						"SELECT _id, name, address, type, notes FROM restaurants WHERE _ID=?",
+						"SELECT _id, name, address, type, notes, feed FROM restaurants WHERE _ID=?",
 						args));
 	}
 
